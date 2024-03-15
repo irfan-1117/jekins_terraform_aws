7d40d832b095b1ea27e9dd4622d228c4e845b808
irfan shaikh <irfan1711@outlook.com>
2024-03-15 12:01:00 +0530

    modifiy terraform files for web server

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
   #count             = length(var.public_subnet_cidr_blocks)
   vpc_id     = aws_vpc.main.id
-  cidr_block = var.public_subnet_cidr_blocks[count.index]
+  cidr_block = var.public_subnet_cidr_blocks
   #availability_zone = element(data.aws_availability_zones.available.names, count.index)
   availability_zone = data.aws_availability_zones.available.names
 
